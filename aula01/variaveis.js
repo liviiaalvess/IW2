c13f507ea25d822cd221c7737fcea93415e27598
Livia Alves <liviaalvesetec25@gmail.com>
2026-02-23 08:45:08 -0300

    Add files via upload

---
+++ aula01/variaveis.js
@@ -0,0 +1,31 @@
+var nomeDavariavel = "valor da variavel";
+var outraVariavel = 'valor da outra variavel';
+var variavelIdade = 22;
+var variavelTenhoCabeloBranco = false;
+var variavelIndefinida ; 
+
+console.log(nomeDavariavel);
+console.log(outraVariavel);
+console.log(variavelIdade);
+console.log(variavelTenhoCabeloBranco);
+console.log(variavelIndefinida);
+
+var a = 14;
+var b = 42;
+var tr = (a == 14);
+var fl = (a == b);
+
+//Neste caso tr irá conter o valor true e fl o valor falso.
+
+var int1 = tr + 1;
+var int2 = fl + 1;
+
+//A variavel int1 irá conter o valor 2 (true mais 1), pois true é 
+//automaticamente convertido para um e int2 irá conter o valor 1 
+//(false +1), pois false é convertido para 0.
+
+console.log(a);
+console.log(b);
+console.log(tr);
+console.log(fl);
+
